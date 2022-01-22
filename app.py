c20f99cb5e0e760aaa50f6e78748fc227fea5c85
galaxine~senpai <insta.is.good1234@gmail.com>
2022-01-21 16:12:04 -0800

    added 3 new error pages (thanks freefrontend.com)

--- app.py
+++ app.py
@@ -11,6 +11,26 @@ def index():
 def team():
     return render_template('team.html')
 
-@app.route('/undefined')
+@app.route('/404')
 def error404():
     return render_template('404.html')
+
+@app.route('/403')
+def error403():
+    return render_template('403.html')
+
+@app.route('/500')
+def error500():
+    return render_template('500.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(403)
+def page_not_allowed(e):
+    return render_template('403.html'), 403
+
+@app.errorhandler(500)
+def page_not_allowed(e):
+    return render_template('500.html'), 500
