a14b26fe73130ae901a998ad6bacac080cce6f67
galaxine~senpai <70488944+galaxine-senpai@users.noreply.github.com>
2022-03-09 19:21:30 -0800

    changed a ton of things

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask import render_template
+from flask import Response
+import random
+randomport = random.randint(2000, 9000)
 
 app = Flask(__name__)
-
+  
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -13,58 +16,70 @@ def team():
 
 @app.route('/404')
 def error404():
-    return render_template('lyric404.html')
+    return render_template('/errors/lyric404.html')
 
 @app.route('/403')
 def error403():
-    return render_template('403.html')
+    return render_template('/errors/403.html')
 
 @app.route('/500')
 def error500():
-    return render_template('500.html')
+    return render_template('/errors/500.html')
 
 @app.route('/408')
 def error408():
-    return render_template('408.html')
+    return render_template('/errors/408.html')
 
 @app.route('/lyric')
 def lyric():
-  return render_template('lyric404.html')
-
-@app.route('/renders')
-def renders():
-  return render_template('renders.html')
-
-@app.route('/renders/supra')
-def supie():
-  return render_template('supie.html')
-
-@app.route('/renders/supra/purpmist')
-def supierender():
-  return render_template('supiepurpmist.html')
-
-@app.route('/renders/supra/blackfog')
-def supierender2():
-  return render_template('supiemistblack.html')
+  return render_template('/errors/lyric404.html')
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+  return render_template('portfolio.html')
+
+@app.route('/robots.txt')
+def robots():
+  return app.send_static_file('robots.txt')
+
+@app.route('/logo.png')
+def logo():
+  return app.send_static_file('bot.png')
+
+@app.route('/school/basic')
+def basic():
+  return render_template('/school/basic.html')
+
+@app.route('/school/page2')
+def page2():
+  return render_template('/school/page2.html')
+
+@app.route('/school/rock_climbing')
+def rock_climbing():
+  return render_template('/school/rock.html')
+
+@app.route('/rock.png')
+def rock_photo():
+  return app.send_static_file('rock.png')
+
+@app.route('/school/assesment')
+def assesment():
+  return render_template('/school/assesment.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('lyric404.html'), 404
+    return render_template('/errors/lyric404.html'), 404
 
 @app.errorhandler(403)
 def page_not_authorized(e):
-    return render_template('403.html'), 403
+    return render_template('/errors/403.html'), 403
 
 @app.errorhandler(500)
 def server_error(e):
-    return render_template('500.html'), 500
+    return render_template('/errors/500.html'), 500
 
 @app.errorhandler(408)
 def server_timeout(e):
-    return render_template('408.html'), 408
+    return render_template('/errors/408.html'), 408
 
-app.run(host='0.0.0.0', port=8080, debug=True)
+app.run(host='0.0.0.0', port=randomport, debug=True)
